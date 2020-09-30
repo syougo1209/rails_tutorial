d4902f911416279409aea1310c73d3e3eeffa142
EC2 Default User <syougo1999most@yahoo.co.jp>
2020-09-30 12:09:32 +0000

    Inplement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
   
-  def hello
-    render html: "hello"
+  def login(user)
+    session[:user_id]=user.id
   end
-    
 end
