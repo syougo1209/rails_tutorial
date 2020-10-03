ad3453073d6e8a877c44ef47378a6467bead5ac7
EC2 Default User <syougo1999most@yahoo.co.jp>
2020-10-03 12:47:35 +0000

    Userにadminを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :index]
   before_action :correct_user, only: [:edit,:update]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def new
     @user=User.new
   end
@@ -18,7 +23,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_back_or @user
     else
       render 'new'
     end
@@ -40,10 +45,10 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-  
-    def logged_in_user
+     def logged_in_user
       unless logged_in?
-        flash[:danger]="Please log in"
+        store_location
+        flash[:danger] = "Please log in."
         redirect_to login_url
       end
     end
@@ -52,4 +57,5 @@ class UsersController < ApplicationController
       @user=User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
+    
 end
