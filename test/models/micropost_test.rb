39965b06fbf61a05b2cb1593909e053b6009f405
EC2 Default User <syougo1999most@yahoo.co.jp>
2020-10-04 06:26:27 +0000

    投稿の順番を修正

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
     # このコードは慣習的に正しくない
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -25,4 +25,10 @@ class MicropostTest < ActiveSupport::TestCase
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
   end
+  
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
+  
 end
+ 
